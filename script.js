4737bf5a04900c6842debabd4190101debdb995a
Jhonathan Santos <jhonathanwallace22@gmail.com>
2024-07-16 16:01:36 -0300

    Menu mobile

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', function() {
+  const menuToggle = document.querySelector('.menu-toggle');
+  const navLinks = document.querySelector('.nav-links');
+
+  menuToggle.addEventListener('click', function() {
+      menuToggle.classList.toggle('open');
+      navLinks.classList.toggle('open');
+  });
+});
