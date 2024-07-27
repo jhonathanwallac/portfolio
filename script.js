9c90e24b91fa210426f2e5756404860dbd4076cd
jhonathanwallac <jhonathanwallace@gmail.com>
2024-07-27 17:00:19 -0300

    Ajustes no projeto em geral(part.3)

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-// script.js
-
 document.addEventListener('DOMContentLoaded', function() {
   const menuToggle = document.querySelector('.menu-toggle');
   const navLinks = document.querySelector('.nav-links');
+  const links = navLinks.querySelectorAll('a'); // Seleciona todos os links dentro do menu
 
   menuToggle.addEventListener('click', function() {
       menuToggle.classList.toggle('open');
       navLinks.classList.toggle('open');
+  });
+
+  // Adiciona um event listener a cada link para fechar o menu ao clicar
+  links.forEach(function(link) {
+    link.addEventListener('click', function() {
+      menuToggle.classList.remove('open');
+      navLinks.classList.remove('open');
+    });
   });
 });
